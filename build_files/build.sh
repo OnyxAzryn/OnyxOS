ef29b79102399bbe26f869f1ed65185190a8b035
OnyxAzryn <brycekabat@onyxazryn.com>
2025-05-10 12:21:28 -0500

    Remove Docker (for now...)

--- build_files/build.sh
+++ build_files/build.sh
@@ -13,7 +13,7 @@ gpgkey=https://packages.microsoft.com/keys/microsoft.asc
 EOF
 
 # Install required packages
-dnf install -y code docker-ce gcc libvirt libvirt-client libvirt-nss virt-manager virt-viewer wireshark zsh
+dnf install -y code gcc libvirt libvirt-client libvirt-nss virt-manager virt-viewer wireshark zsh
 
 # Uninstall Firefox, use the Flatpak instead
 dnf remove -y firefox firefox-langpacks
